a4b73222a24ba473e477add6352f8a7f8cd422f7
Robert Spanton <rspanton@zepler.net>
2010-09-17 16:27:16 +0100

    Add a diagram showing the ramp entrance gaffer and blue dots

--- images/include.mk
+++ images/include.mk
@@ -1,5 +1,5 @@
 images_targets := $(addprefix images/,arena.pdf flag-2011.pdf quadrants.pdf ramp.pdf \
-		ir-mount.pdf)
+		ir-mount.pdf ramp-entrance.pdf)
 TARGETS += $(images_targets)
 CLEAN += $(images_targets)
 
