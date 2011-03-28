1c9dcef921e8dbaf1bac8ecefaba2a75a1482399
Peter Law <PeterJCLaw@gmail.com>
2011-03-28 20:47:13 +0100

    whitespace

--- fr/include.mk
+++ fr/include.mk
@@ -1,5 +1,5 @@
 TARGETS += fr/rulebook.pdf
-CLEAN += $(addprefix fr/*.,pdf aux dvi log) 
+CLEAN += $(addprefix fr/*.,pdf aux dvi log)
 
 fr/rulebook.pdf: $(addprefix fr/,rulebook.tex game-rules.tex regs.tex specs.tex kit-return.tex awards.tex) $(images_targets)
 	TEXINPUTS=".:fr/:" pdflatex -output-directory fr fr/rulebook.tex
