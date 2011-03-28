58f0bab8f5d8a30d5a46cf45427b016c7ff025ec
Peter Law <PeterJCLaw@gmail.com>
2011-03-28 20:47:59 +0100

    Ensure that the links in the files are resolved.
    The internal links need a second run against tex so resolve them.

--- en/include.mk
+++ en/include.mk
@@ -3,3 +3,4 @@ CLEAN += $(addprefix en/*.,pdf aux dvi log)
 
 en/rulebook.pdf: $(addprefix en/,rulebook.tex game-rules.tex regs.tex specs.tex kit-return.tex awards.tex) $(images_targets)
 	TEXINPUTS=".:en/:" pdflatex -output-directory en en/rulebook.tex
+	TEXINPUTS=".:en/:" pdflatex -output-directory en en/rulebook.tex
--- fr/include.mk
+++ fr/include.mk
@@ -3,3 +3,4 @@ CLEAN += $(addprefix fr/*.,pdf aux dvi log)
 
 fr/rulebook.pdf: $(addprefix fr/,rulebook.tex game-rules.tex regs.tex specs.tex kit-return.tex awards.tex) $(images_targets)
 	TEXINPUTS=".:fr/:" pdflatex -output-directory fr fr/rulebook.tex
+	TEXINPUTS=".:fr/:" pdflatex -output-directory fr fr/rulebook.tex
