3e2e7cf764dc442c675d07bec3ab77cc1c497bb6
Robert Spanton <rspanton@zepler.net>
2011-09-28 23:07:29 +0100

    Include the bird's-eye view in the rulebook

--- images/include.mk
+++ images/include.mk
@@ -1,4 +1,4 @@
-images_targets := $(addprefix images/,flag-2011.pdf quadrants.pdf ramp.pdf \
+images_targets := $(addprefix images/,arena.pdf flag-2011.pdf quadrants.pdf ramp.pdf \
 		ir-mount.pdf ramp-entrance.pdf internal-wall.pdf)
 TARGETS += $(images_targets)
 CLEAN += $(images_targets)
