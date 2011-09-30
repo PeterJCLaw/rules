7f27ccb183f295c1d7f0aed039eef0ba4667aa20
Robert Spanton <rspanton@zepler.net>
2011-09-30 02:15:45 +0100

    Add beginnings of a 3D diagram of a bucket

--- images/include.mk
+++ images/include.mk
@@ -1,5 +1,5 @@
-images_targets := $(addprefix images/,arena.pdf flag-2011.pdf quadrants.pdf ramp.pdf \
-		ir-mount.pdf ramp-entrance.pdf internal-wall.pdf)
+images_targets := $(addprefix images/,arena.pdf flag-2011.pdf quadrants.pdf \
+		sidewall.pdf arena-markers.pdf bucket-3d.pdf)
 TARGETS += $(images_targets)
 CLEAN += $(images_targets)
 
