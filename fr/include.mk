474bf4bd0595dd071fad8f3cb8d0dbb10ffcdb92
Peter Law <PeterJCLaw@gmail.com>
2012-04-17 20:49:35 +0100

    Remove the french translation.
    This is now two years out of date, and is just getting in the way.
    If anyone feels like creating a translation in the future, it will
    probably be quicker to clone the up-to-date english anyway.

--- fr/include.mk
+++
@@ -1,6 +0,0 @@
-TARGETS += fr/rulebook.pdf
-CLEAN += $(addprefix fr/*.,pdf aux dvi log)
-
-fr/rulebook.pdf: $(addprefix fr/,${FILES}) $(images_targets)
-	TEXINPUTS=".:fr/:" pdflatex -output-directory fr fr/rulebook.tex
-	TEXINPUTS=".:fr/:" pdflatex -output-directory fr fr/rulebook.tex
--- makefile
+++ makefile
@@ -1,4 +1,4 @@
-all: en/rulebook.pdf fr/rulebook.pdf
+all: en/rulebook.pdf
 
 TARGETS :=
 CLEAN :=
