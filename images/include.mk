80a5eb414e61bb44bc62e61e96353b47278c6118
Alastair Lynn <arplynn@gmail.com>
2012-11-01 17:23:07 +0000

    Fix the arena.pdf display
    Previously, inkscape was incorrectly rendering the PDF from the SVG
    source. I've manually converted the SVG to a PDF and included it here.

--- images/include.mk
+++ images/include.mk
@@ -1,4 +1,4 @@
-images_targets := $(addprefix images/,arena.pdf flag-2011.pdf quadrants.pdf \
+images_targets := $(addprefix images/,flag-2011.pdf quadrants.pdf \
 		sidewall.pdf arena-markers.pdf bucket-3d.pdf barrier-feet.pdf \
 		robot-marker.pdf badge-mounting.pdf )
 TARGETS += $(images_targets)
