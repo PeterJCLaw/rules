a43af4cdce8840e2764bbccf2ad3996ef8680b89
Chris Kirkham <chrisjameskirkham@gmail.com>
2013-10-05 22:22:39 +0100

    Add sections on zones and slots inc. a new diagram
    This will probably require polishing when the construction of the
    wall and slots is considered a little more.

--- images/include.mk
+++ images/include.mk
@@ -1,6 +1,7 @@
 images_targets := $(addprefix images/,arena.pdf flag-2011.pdf quadrants.pdf \
 		sidewall.pdf arena-markers.pdf bucket-3d.pdf barrier-feet.pdf \
-		robot-marker.pdf badge-mounting.pdf token-position.pdf)
+		robot-marker.pdf badge-mounting.pdf token-position.pdf \
+		slots-zones.pdf)
 TARGETS += $(images_targets)
 CLEAN += $(images_targets)
 
