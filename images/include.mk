745a0880a3015c87f210331d28f24d2a9843d365
Alistair Lynn <arplynn@gmail.com>
2014-11-20 21:53:08 +0000

    Remove unused images
    These are related to previous competitions are neither used nor
    relevant.
    
    This doesn't affect the generated PDF, so no revision or changelog
    bump.

--- images/include.mk
+++ images/include.mk
@@ -1,7 +1,6 @@
-images_targets := $(addprefix images/,arena.pdf flag-2011.pdf quadrants.pdf \
-		sidewall.pdf arena-markers.pdf bucket-3d.pdf barrier-feet.pdf \
-		robot-marker.pdf badge-mounting.pdf token-position.pdf \
-		slots-zones.pdf)
+images_targets := $(addprefix images/,arena.pdf \
+		sidewall.pdf arena-markers.pdf \
+		robot-marker.pdf badge-mounting.pdf)
 TARGETS += $(images_targets)
 CLEAN += $(images_targets)
 
