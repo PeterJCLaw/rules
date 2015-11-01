b1ef0114ec4ce466488348d7a3d39c510164967c
Ed Longman <e.i.a.longman@gmail.com>
2015-11-01 16:36:40 +0000

    Create 3 different nets of cubes
    Patch set 2: Fit the images' pages to the diagrams, get them built.
    
    Change-Id: I536b15225e1d39f6f6179f273ccdb368239d538e

--- images/include.mk
+++ images/include.mk
@@ -1,4 +1,5 @@
 images_targets := $(addprefix images/,arena.pdf \
+		net-1.pdf net-2.pdf net-3.pdf \
 		sidewall.pdf arena-markers.pdf \
 		robot-marker.pdf badge-mounting.pdf)
 TARGETS += $(images_targets)
