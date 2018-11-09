16c09bad54872c39bd220a3860592b1b61e5481b
Alistair Lynn <arplynn@gmail.com>
2018-11-09 21:05:24 +0000

    Drop the `include.mk`

--- en/include.mk
+++
@@ -1,6 +0,0 @@
-TARGETS += en/rulebook.pdf
-CLEAN += $(addprefix en/*.,pdf aux dvi log out fff ttt)
-
-en/rulebook.pdf: $(addprefix en/,${FILES}) $(images_targets)
-	TEXINPUTS=".:en/:" pdflatex -output-directory en en/rulebook.tex
-	TEXINPUTS=".:en/:" pdflatex -output-directory en en/rulebook.tex
